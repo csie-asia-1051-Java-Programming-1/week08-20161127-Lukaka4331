41641de82ddfd31a3f883bbd0497c4ec266dcfb3
Lukaka4331 <105021044@live.asia.edu.tw>
2016-11-22 12:50:06 +0800

    2pp

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,15 +1,28 @@
-﻿package ex;
+﻿package ex;import java.util.Scanner;
 /*
  * Topic: 將前一題加強成可以做10以下的各種數字系統轉換
  * Date: 2016/11/21
- * Author: 1050210XX 周永振老師
+ * Author: 105021044 鐘岑哲
  */
 
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+		  Scanner scn =new Scanner(System.in);
+		   int a=scn.nextInt();
+		   
+		   System.out.println(verb(a));}
+	
+	
+    public static String verb (int a){  Scanner scn =new Scanner(System.in);
+    	String b="";int c=scn.nextInt();while(a>0){
+    		b=Integer.toString(a%c)+b;
+    	    a/=c;	 
+    	    	 
+    	    	 
+    	    	 
+    	    	 
+    	}return b;
+}
 }
